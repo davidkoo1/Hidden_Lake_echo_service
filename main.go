4d13f910700fd20a50fc9d7f83c6f09a83c12f23
davidkoo1 <sverceks@gmail.com>
2024-08-04 23:32:55 +0300

    Simple echo http method

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
-package main 
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello world");
-
+	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
+		req, err := io.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		fmt.Fprintf(w, "echo(%s)", string(req))
+	})
+	log.Println("Service is Listening...")
+	http.ListenAndServe("127.0.0.1:8080", nil)
 }
